ae9b0d24f0b2381ac51e984bc7b1943ddfe70a53
gcarniel <gpcarniel@outlook.com>
2021-07-13 21:08:17 -0300

    criação das comunidades.

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,44 @@
+import React from 'react'
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
 
 function ProfileSidebar(props) {
-  console.log(props)
   return (
-    <Box>
+    <Box as="aside">
       <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '8px' }} />
+      <hr />
+
+      <p>
+        <a className="boxLink" href={`https://github.com/${props.githubUser}`}>
+          @{props.githubUser}
+        </a>
+      </p>
+
+      <hr />
+
+      <AlurakutProfileSidebarMenuDefault />
     </Box>
   )
 }
 
 export default function Home() {
-  const githubUser = 'gcarniel'
-  const pessoasFavoritas = ['gabriel', 'cariza', 'leonardo', 'nica', 'tunico']
+  const [comunidades, setComunidades] = React.useState([{
+    id: '1234568764452313246653352468736587',
+    title: 'Eu odeio acordar cedo',
+    image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg'
+  }])
+
+  const usuarioGithub = 'gcarniel'
+  const pessoasFavoritas = ['omariosouto', 'peas', 'marcobrunodev', 'juunegreiros']
 
   return (
     <>
-      <AlurakutMenu />
+      <AlurakutMenu githubUser={usuarioGithub} />
       <MainGrid>
         <div className="profileArea" style={{ gridArea: 'profileArea' }}>
-          <ProfileSidebar githubUser={githubUser} />
+          <ProfileSidebar githubUser={usuarioGithub} />
         </div>
 
         <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
@@ -29,11 +46,69 @@ export default function Home() {
             <h1 className="title">
               Bem-vindo(a)
             </h1>
-            
+
             <OrkutNostalgicIconSet />
+          </Box>
+
+          <Box>
+            <h2 className="subTitle" >O que você deseja fazer?</h2>
+
+            <form onSubmit={function handleCriaComunidade(event) {
+              event.preventDefault()
+              const dadosDoForm = new FormData(event.target)
+
+              const comunidade = {
+                id: new Date().toISOString(),
+                title: dadosDoForm.get('title'),
+                image: dadosDoForm.get('image')
+              }
+
+              const comunidadesAtualizadas = [...comunidades, comunidade]
+
+              setComunidades(comunidadesAtualizadas)
+              console.log(comunidades)
+            }} >
+              <div>
+                <input
+                  placeholder="Qual vai ser o nome da sua comunidade?"
+                  type="text"
+                  name="title"
+                  aria-label="Qual vai ser o nome da sua comunidade?" />
+              </div>
+
+              <div>
+                <input
+                  placeholder="Coloque uma URL para usarmos de capa."
+                  type="text"
+                  name="image"
+                  aria-label="Coloque uma URL para usarmos de capa." />
+              </div>
+
+              <button>
+                Criar Comunidade
+              </button>
+            </form>
+
           </Box>
         </div>
         <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Comunidades ({comunidades.length})
+            </h2>
+            <ul>
+              {comunidades.map((itemAtual) => {
+                return (
+                  <li key={itemAtual.id}>
+                    <a href={`/users/${itemAtual.title}`}>
+                      <img src={itemAtual.image} />
+                      <span>{itemAtual.title}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
           <ProfileRelationsBoxWrapper>
             <h2 className="smallTitle">
               Pessoas da comunidade ({pessoasFavoritas.length})
